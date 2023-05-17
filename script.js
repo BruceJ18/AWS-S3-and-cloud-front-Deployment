617fa00a73917c9bcab8702966385796d83de21b
Bruce Jacobo <bruceja18@protonmail.com>
2023-05-17 14:52:08 -0700

    Implement responsive design.

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 // drop down menues: below
 
-document.addEventListener('click', e => {
-    const isDropDownButton = e.target.matches("[data-dropdown-button]");
-    if (!isDropDownButton && e.target.closest("[data-dropdown]") != null) return
+document.addEventListener('click', (event) => {
+    const isDropDownButton = event.target.matches("[data-dropdown-button]");
+    if (!isDropDownButton && event.target.closest("[data-dropdown]") != null) return
 
     let currentDropDown;
     if (isDropDownButton) {
-        currentDropDown = e.target.closest("[data-dropdown]")
+        currentDropDown = event.target.closest("[data-dropdown]")
         currentDropDown.classList.toggle("active")
     }
-
+    
     document.querySelectorAll("[data-dropdown].active").forEach(dropDown => {
         if ( dropDown === currentDropDown) return
         dropDown.classList.remove("active")
     })
 }) 
+
+document.addEventListener('click', (event) => {
+    const isDropDownButton2 = event.target.matches("[data-dropdown-button-2]");
+    if (!isDropDownButton2 && event.target.closest("[data-dropdown-2]") != null) return
+
+    let currentDropDown;
+    if (isDropDownButton2) {
+        currentDropDown = event.target.closest("[data-dropdown-2]")
+        currentDropDown.classList.toggle("active")
+    }
+
+    document.querySelectorAll("[data-dropdown-2].active").forEach(dropDown => {
+        if ( dropDown === currentDropDown) return
+        dropDown.classList.remove("active")
+    })
+}) 
+
 
 // logo click effect: below
 
